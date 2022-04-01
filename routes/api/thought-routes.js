92e0fc0ff6b77eb61532f4fae1279c6aea589b6b
Yashar <yasharjs@gmail.com>
2022-03-31 22:00:04 -0400

    added routes for the models

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,3 +1,24 @@
 const router = require("express").Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  addThought,
+  removeThought,
+  updateThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thought-controller");
+
+// /api/thoughts
+router.route("/").get(getAllThoughts).post(addThought);
+
+// /api/thoughts/:thoughtId
+router.route("/:thoughtId").get(getThoughtById).put(updateThought).delete(removeThought);
+
+// POST to /api/thoughts/:thoughtsId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
 
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,3 +1,21 @@
 const router = require("express").Router();
-
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    addFriend,
+    deleteFriend,
+    updateUser,
+    deleteUser,
+  } = require("../../controllers/user-controller");
+  
+  // /api/users
+  router.route("/").get(getAllUsers).post(createUser);
+  
+  // /api/users/:id
+  router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser);
+  
+  // /api/users/:userId/friends/:friendId
+  router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
+  
 module.exports = router;
